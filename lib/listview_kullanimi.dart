37b980d878de69475815427ab6e45b08c9ffb91e
Sinan <sinanfen@hotmail.com>
2023-05-13 05:49:51 +0300

    ListView build and seperated

--- lib/listview_kullanimi.dart
+++ lib/listview_kullanimi.dart
@@ -6,7 +6,7 @@ class ListViewKullanimi extends StatelessWidget {
   ListViewKullanimi({super.key});
 
   List<Ogrenci> tumOgrenciler = List.generate(
-    500,
+    1000,
     (index) =>
         Ogrenci(index + 1, 'Öğrenci adı: ${index + 1}', 'Öğrenci soyadı: ${index + 1}'),
   );
@@ -17,18 +17,55 @@ class ListViewKullanimi extends StatelessWidget {
       appBar: AppBar(
         title: Text('Öğrenci Listesi'),
       ),
-      body: ListView(
-        children: tumOgrenciler
-            .map((Ogrenci ogrenci) => ListTile(
-                  title: Text(ogrenci.isim),
-                  subtitle: Text(ogrenci.soyisim),
-                  leading: CircleAvatar(
-                      child: Icon(
-                    Icons.person_2_rounded,
-                  )),
-                ))
-            .toList(),
-      ),
+      body: buildListViewSeperated(),
+    );
+  }
+
+  ListView buildListViewSeperated() {
+    return ListView.separated(
+      itemBuilder: (BuildContext context, int index) {
+        var nowOgrenci = tumOgrenciler[index];
+        return Card(
+          color: index % 2 == 0 ? Colors.orange.shade100 : Colors.purple.shade100,
+          child: ListTile(
+            onTap: () {
+              print('Eleman tıklandı: ${index}');
+            },
+            title: Text(nowOgrenci.isim),
+            subtitle: Text(nowOgrenci.soyisim),
+            leading: CircleAvatar(
+                child: Icon(
+              Icons.person_2_rounded,
+            )),
+          ),
+        );
+      },
+      itemCount: tumOgrenciler.length,
+      separatorBuilder: (context, index) {
+        var yeniIndex = index + 1;
+        if (yeniIndex % 4 == 0) {
+          return Divider(
+            thickness: 1,
+            color: Colors.teal,
+          );
+        }
+        return SizedBox();
+      },
+    );
+  }
+
+  ListView klasikListView() {
+    return ListView(
+      children: tumOgrenciler
+          .map((Ogrenci ogrenci) => ListTile(
+                title: Text(ogrenci.isim),
+                subtitle: Text(ogrenci.soyisim),
+                leading: CircleAvatar(
+                    child: Icon(
+                  Icons.person_2_rounded,
+                )),
+              ))
+          .toList(),
     );
   }
 }
